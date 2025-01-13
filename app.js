d15f74e796d23d36572169f357b9f9b315204f03
o0w0o-dev <o0w0o.dev@gmail.com>
2025-01-13 23:22:13 +0800

    feat: apply userRouter

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { router as imageRouter } from "./routes/imageRoutes.js";
+import { router as userRouter } from "./routes/userRoutes.js";
 
 const app = express();
 const port = 4000;
 
 app.use("/api/v1/images", imageRouter);
+app.use("/api/v1/users", userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ root: true });
