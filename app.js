bf3b03e732c758b8e45649f0ac4d51fda8df5ee5
o0w0o-dev <o0w0o.dev@gmail.com>
2025-01-13 22:37:37 +0800

    feat: apply imageRouter

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require("express");
+import express from "express";
+import { router as imageRouter } from "./routes/imageRoutes.js";
+
 const app = express();
 const port = 4000;
 
+app.use("/api/v1/images", imageRouter);
+
 app.get("/", (req, res) => {
   res.status(200).json({ root: true });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Cannot find ${req.originalUrl} on this server.`,
+  });
 });
 
 app.listen(port, () => {
