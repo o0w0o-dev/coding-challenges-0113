7dc42b3507efa646411a59f3210883ffbe48a5e8
o0w0o-dev <o0w0o.dev@gmail.com>
2025-01-13 19:29:31 +0800

    feat: add unsplash

---
+++ utils/unsplash.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
+if (!UNSPLASH_API_KEY) throw new Error("UNSPLASH_API_KEY is undefined");
+
+const searchImages = async function (query) {
+  try {
+    const url = `https://api.unsplash.com/search/photos?query=${query}`;
+    const headers = {
+      "Accept-Version": "v1",
+      "Content-Type": "application/json",
+      Authorization: `Client-ID ${UNSPLASH_API_KEY}`,
+    };
+
+    const response = await fetch(url, { method: "GET", headers });
+    if (response.ok) {
+      const data = await response.json();
+      const output = data?.results?.map((item) => ({
+        image_ID: item.id,
+        thumbnails: item.urls.thumb,
+        preview: item.urls.regular,
+        title: item.description,
+        source: "Unsplash",
+        tags: [],
+      }));
+      return output;
+    } else {
+      return [];
+    }
+  } catch (err) {
+    console.log(`Fetch unsplash api error: ${err}`);
+    return [];
+  }
+};
+
+export { searchImages };
