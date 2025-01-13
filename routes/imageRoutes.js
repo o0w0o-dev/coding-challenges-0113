3d0dc05d901752a288416004197e42b723ec1305
o0w0o-dev <o0w0o.dev@gmail.com>
2025-01-13 23:21:47 +0800

    feat: add protect to imageRoutes

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,9 +1,12 @@
 "use strict";
 
 import express from "express";
+import { protect } from "../controllers/authController.js";
 import { searchImages } from "../controllers/imageController.js";
 
 const router = express.Router();
+
+router.use(protect);
 
 router.get("/", searchImages);
 
