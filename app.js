9c59ce4a8b73d0dc4c6fdf293b74f755bbcf97f0
o0w0o-dev <o0w0o.dev@gmail.com>
2025-01-14 08:38:44 +0800

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
 import { router as imageRouter } from "./routes/imageRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+const DB_URI = process.env.DB_URI;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.use("/api/v1/images", imageRouter);
 app.use("/api/v1/users", userRouter);
